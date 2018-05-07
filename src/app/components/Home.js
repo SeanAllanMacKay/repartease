773a62f63c40cc1816d7adb75d5c37dd01985e27
SeanAllanMacKay <seanallanmackay@gmail.com>
2018-05-07 14:03:15 -0700

    changed home page styling

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,20 +4,37 @@ import { Button } from 'semantic-ui-react'
 
 export class Home extends React.Component{
 	render(){
+
+		const containerStyle = {
+			width: 80 + '%',
+			marginLeft: 10 + '%',
+			marginTop: 40 + '%'
+		}
+
+		const buttonStyle = {
+			marginTop: 15 + '%',
+			marginBottom: 15 + '%'
+		}
+
 		const {
 			forward
 		}=this.props
 
 	    return (
 	    	<Root>
-		        <div>
+		        <div style={containerStyle}>
 					<Button 
+					fluid
+					size = 'massive'
+					style={buttonStyle}
 					onClick={()=>{forward(this.props.history, '/start-game')}}>
 						Start Game
 					</Button>
-				</div>
-				<div>
+
 					<Button 
+					fluid
+					size = 'massive'
+					style={buttonStyle}
 					onClick={()=>{forward(this.props.history, '/join-game')}}>
 						Join Game
 					</Button>
