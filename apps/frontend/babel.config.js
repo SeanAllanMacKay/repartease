87b1aca9a31bc06b82d63a28b79e86b9444cf904
SeanMacKay <seanallanmackay@gmail.com>
2024-05-18 16:57:30 -0500

    Fix issues

--- apps/frontend/babel.config.js
+++ apps/frontend/babel.config.js
@@ -2,6 +2,27 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          extensions: [
+            ".js",
+            ".jsx",
+            ".json",
+            ".ts",
+            ".tsx",
+          ],
+          root: ["."],
+          alias: {
+            Contexts: "./Contexts",
+            Components: "./Components",
+            BIC: "./BIC",
+            Api: "./Api",
+          },
+        },
+      ],
+    ],
   };
 };
--- apps/frontend/components/Icon.tsx
+++ apps/frontend/components/Icon.tsx
@@ -1,7 +1,5 @@
 import React, { useMemo } from "react";
 
-import {} from "@phosphor-icons/react";
-
 export const iconMapping = {};
 
 export const Icon = ({
